ee093e7c0a2813030028008a8c0a8eb2bc452a1b
lujunjian <670214261@qq.com>
2020-04-29 16:51:33 +0800

    404页面

--- src/projects/cms/router/index.js
+++ src/projects/cms/router/index.js
@@ -17,12 +17,32 @@ const routes = [
     path: '/my-center',
     name: 'myCenter',
     component: () => import('$cms/views/myCenter'),
-  }
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: () => import('$cms/views/404'),
+  },
+
+  //必须放到最后面
+  { path: '*', redirect: '/404', hidden: true }
 ]
 
 const router = new VueRouter({
   // mode: 'history',
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    // keep-alive 返回缓存页面后记录浏览位置
+    if (savedPosition && to.meta.keepAlive) {
+      return savedPosition
+    }
+    // 异步滚动操作
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ x: 0, y: 0 })
+      }, 0)
+    })
+  }
 })
 
 export default router
---
+++ src/projects/cms/views/404/index.vue
@@ -0,0 +1,19 @@
+/**
+* Created by LuJunJian on 2020/4/29
+* 404页面
+*/
+<template>
+  <div>
+    404页面
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
